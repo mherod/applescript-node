3dd970d2ad76039a47f2976a24ca4c8205ce9cea
Matthew Herod <matthew.herod@myunidays.com>
2024-12-06 02:09:23 +0000

    fix: update tsup dts configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  dts: true,
+  dts: {
+    entry: 'src/index.ts',
+    resolve: true,
+  },
   splitting: false,
   sourcemap: true,
   clean: true,
